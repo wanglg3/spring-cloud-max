0c1ea39e6c2889a98d0cf896e55925e0c0b42056
wanglg3 <wanglg3@163.com>
2025-02-10 21:47:54 +0800

    :tada: oauth2.1测试类

---
+++ spring-cloud-max-passport/src/main/java/cloud/max/passport/SpringCloudMaxPassportApplication.java
@@ -0,0 +1,14 @@
+package cloud.max.passport;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudMaxPassportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudMaxPassportApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-max-passport/src/test/java/cloud/max/passport/SpringCloudMaxPassportApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.max.passport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudMaxPassportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
