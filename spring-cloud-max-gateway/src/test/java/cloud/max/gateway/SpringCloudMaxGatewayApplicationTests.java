45457140e9b30a535bfb333485189c2e66576819
wlg <279733389@qq.com>
2025-02-10 09:59:22 +0800

    :tada init

---
+++ spring-cloud-max-gateway/src/test/java/cloud/max/gateway/SpringCloudMaxGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.max.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudMaxGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
